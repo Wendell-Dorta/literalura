0162a8bb379d061182892b5dc1114025e5770a3a
Wendell Dorta <wendelldorta.ws@outlook.com.br>
2025-07-30 18:32:20 -0300

    criando o model de Autor

---
+++ src/main/java/br/com/alura_challenge/literalura/model/Author.java
@@ -0,0 +1,40 @@
+package br.com.alura_challenge.literalura.model;
+
+public class Author {
+    private Integer id;
+    private String name;
+    private String birth_year;
+    private String death_year;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBirth_year() {
+        return birth_year;
+    }
+
+    public void setBirth_year(String birth_year) {
+        this.birth_year = birth_year;
+    }
+
+    public String getDeath_year() {
+        return death_year;
+    }
+
+    public void setDeath_year(String death_year) {
+        this.death_year = death_year;
+    }
+}
