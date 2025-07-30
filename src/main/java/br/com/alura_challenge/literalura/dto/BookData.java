df2f04fda00ab850fe9c3bc0625bfd12525d840e
Wendell Dorta <wendelldorta.ws@outlook.com.br>
2025-07-30 20:16:30 -0300

    fix(dto): corrigindo o tipo de authors para List<AuthorData>

--- src/main/java/br/com/alura_challenge/literalura/dto/BookData.java
+++ src/main/java/br/com/alura_challenge/literalura/dto/BookData.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public record BookData(
         @JsonAlias("title")String title,
-        @JsonAlias("authors")List<String> authors,
+        @JsonAlias("authors")List<AuthorData> authors,
         @JsonAlias("languages")List<String> languages,
         @JsonAlias("download_count")Integer downloadsNumber
 ) {
