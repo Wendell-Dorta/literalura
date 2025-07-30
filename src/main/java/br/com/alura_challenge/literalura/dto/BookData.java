6e69436e7a682329c74bc830208f4a17d75d5b92
Wendell Dorta <wendelldorta.ws@outlook.com.br>
2025-07-30 20:45:09 -0300

    fix(BookData): mudando o @JsonIgnoreProperties para true

--- src/main/java/br/com/alura_challenge/literalura/dto/BookData.java
+++ src/main/java/br/com/alura_challenge/literalura/dto/BookData.java
@@ -3,10 +3,12 @@ package br.com.alura_challenge.literalura.dto;
 import br.com.alura_challenge.literalura.model.Author;
 import br.com.alura_challenge.literalura.model.Language;
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record BookData(
         @JsonAlias("title")String title,
         @JsonAlias("authors")List<AuthorData> authors,
