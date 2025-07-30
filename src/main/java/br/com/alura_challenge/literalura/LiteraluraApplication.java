a684487b8a353322b4142eb604a9ba10310ea050
Wendell Dorta <wendelldorta.ws@outlook.com.br>
2025-07-30 17:17:12 -0300

    criando e exibindo menu

--- src/main/java/br/com/alura_challenge/literalura/LiteraluraApplication.java
+++ src/main/java/br/com/alura_challenge/literalura/LiteraluraApplication.java
@@ -1,13 +1,19 @@
 package br.com.alura_challenge.literalura;
 
+import br.com.alura_challenge.literalura.main.Menu;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiteraluraApplication {
-
+public class LiteraluraApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Menu menu = new Menu();
+		menu.showMenu();
+	}
 }
---
+++ src/main/java/br/com/alura_challenge/literalura/main/Menu.java
@@ -0,0 +1,52 @@
+package br.com.alura_challenge.literalura.main;
+
+import java.util.Scanner;
+
+public class Menu {
+    private Scanner leitura = new Scanner(System.in);
+
+    public void showMenu(){
+        var opcao = -1;
+
+        while (opcao != 0) {
+            var menu = """
+                    -----------------------------------------------
+                    1 - Buscar livro pelo título
+                    2 - Buscar livros registrados
+                    3 - Listar autores registrados
+                    4 - Buscar autores vivos em um determinado ano
+                    5 - Listar livros em um determinado idioma
+                    -----------------------------------------------
+                    0 - Sair
+                    -----------------------------------------------
+                    """;
+
+            System.out.println(menu);
+            opcao = leitura.nextInt();
+            leitura.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    //searchBookByTitle();
+                    break;
+                case 2:
+                    //searchRegisteredBooks();
+                    break;
+                case 3:
+                    //listRegisteredAuthors();
+                    break;
+                case 4:
+                    //searchLivingAuthorsByYear();
+                    break;
+                case 5:
+                    //searchBooksByLanguage();
+                    break;
+                case 0:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+            }
+        }
+    }
+}
