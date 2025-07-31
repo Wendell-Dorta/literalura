7692bf6a366018fbe4043b3fd9645d8f7be8b748
Wendell Dorta <wendelldorta.ws@outlook.com.br>
2025-07-31 18:16:12 -0300

    refatora(servico): Centraliza persistência em AuthorService e LanguageService

--- src/main/java/br/com/alura_challenge/literalura/service/AuthorService.java
+++ src/main/java/br/com/alura_challenge/literalura/service/AuthorService.java
@@ -1,27 +1,41 @@
 package br.com.alura_challenge.literalura.service;
 
+import br.com.alura_challenge.literalura.dto.AuthorData;
 import br.com.alura_challenge.literalura.model.Author;
 import br.com.alura_challenge.literalura.repository.AuthorRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AuthorService {
-    private final CommonServiceHelper helper;
+    private final GutendexApiHelper helper;
+    private final AuthorRepository repository;
 
-    public AuthorService(CommonServiceHelper helper) {
+    public AuthorService(GutendexApiHelper helper, AuthorRepository repository) {
         this.helper = helper;
+        this.repository = repository;
     }
 
-    @Autowired
-    private AuthorRepository authorRepository;
+    @Transactional
+    public List<Author> findAndSaveAuthors(List<AuthorData> authorDataList) {
+        // 1. Encontra ou cria os objetos em memória
+        List<Author> authors = authorDataList.stream()
+                .map(data -> repository.findByName(data.name())
+                        .orElseGet(() -> new Author(data.name(), data.birthYear(), data.deathYear())))
+                .collect(Collectors.toList());
 
-    @Transactional(readOnly = true)
+        return repository.saveAll(authors);
+    }
+
     public void listRegisteredAuthors() {
-        List<Author> authors = authorRepository.findAll();
-        authors.forEach(System.out::println);
+        List<Author> authors = repository.findAll();
+        if (authors.isEmpty()) {
+            System.out.println("Nenhum autor registrado no banco de dados.");
+        } else {
+            authors.forEach(System.out::println);
+        }
     }
 }
---
+++ src/main/java/br/com/alura_challenge/literalura/service/LanguageService.java
@@ -0,0 +1,30 @@
+package br.com.alura_challenge.literalura.service;
+
+import br.com.alura_challenge.literalura.model.Language;
+import br.com.alura_challenge.literalura.repository.LanguageRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class LanguageService {
+    private final GutendexApiHelper helper;
+    private final LanguageRepository repository;
+
+    public LanguageService(GutendexApiHelper helper, LanguageRepository repository) {
+        this.helper = helper;
+        this.repository = repository;
+    }
+
+    @Transactional
+    public List<Language> findAndSaveLanguages(List<String> languageCodes) {
+        List<Language> languages = languageCodes.stream()
+                .map(code -> repository.findByCode(code)
+                        .orElseGet(() -> new Language(code)))
+                .collect(Collectors.toList());
+
+        return repository.saveAll(languages);
+    }
+}
