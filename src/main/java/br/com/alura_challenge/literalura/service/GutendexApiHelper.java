a9dd2d58efac170d4aa5c288231f2d5e7edfab75
Wendell Dorta <wendelldorta.ws@outlook.com.br>
2025-07-31 16:26:29 -0300

    refactor: Renomeia CommonServiceHelper para GutendexApiHelper

--- src/main/java/br/com/alura_challenge/literalura/service/GutendexApiHelper.java
+++ src/main/java/br/com/alura_challenge/literalura/service/GutendexApiHelper.java
@@ -1,22 +1,17 @@
 package br.com.alura_challenge.literalura.service;
 
-import br.com.alura_challenge.literalura.repository.AuthorRepository;
-import br.com.alura_challenge.literalura.repository.BookRepository;
-import br.com.alura_challenge.literalura.repository.LanguageRepository;
 import br.com.alura_challenge.literalura.util.ConsumptionApi;
 import br.com.alura_challenge.literalura.util.ConvertData;
 import org.springframework.stereotype.Component;
 
-import java.util.Scanner;
-
 @Component
-public class CommonServiceHelper {
+public class GutendexApiHelper {
     private final ConsumptionApi api;
     private final ConvertData convertData;
     private final String ENDERECO = "https://gutendex.com/books/?";
 
-    public CommonServiceHelper(ConsumptionApi api,
-                               ConvertData convertData) {
+    public GutendexApiHelper(ConsumptionApi api,
+                             ConvertData convertData) {
         this.api = api;
         this.convertData = convertData;
     }
