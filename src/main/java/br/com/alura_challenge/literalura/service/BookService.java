b9adc47a8e7d6d969ca9097c1e25a4a0e4617bfd
Wendell Dorta <wendelldorta.ws@outlook.com.br>
2025-07-31 16:33:53 -0300

    refactor: Desacopla dependências e refatora o Helper de serviço

--- src/main/java/br/com/alura_challenge/literalura/service/BookService.java
+++ src/main/java/br/com/alura_challenge/literalura/service/BookService.java
@@ -4,44 +4,44 @@ import br.com.alura_challenge.literalura.dto.BookData;
 import br.com.alura_challenge.literalura.dto.BookResultsData;
 import br.com.alura_challenge.literalura.mapper.BookMapper;
 import br.com.alura_challenge.literalura.model.Book;
+import br.com.alura_challenge.literalura.repository.BookRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BookService {
-    private final CommonServiceHelper helper;
+    private final GutendexApiHelper helper;
+    private final BookRepository repository;
 
-    public BookService(CommonServiceHelper helper) {
+    public BookService(GutendexApiHelper helper, BookRepository repository) {
         this.helper = helper;
+        this.repository = repository;
     }
 
-    private BookData getBookData() {
-        System.out.println("Digite o nome de um livro para buscar");
-        var bookName = helper.getScanner().nextLine();
-
+    private BookData getBookData(String bookName) {
         String url = helper.getEndereco() + "search=" + bookName.replace(" ", "+");
         var json = helper.getApi().getData(url);
         BookResultsData resultsData = helper.getConvertData().getData(json, BookResultsData.class);
 
         if (resultsData.results().isEmpty()) {
             throw new RuntimeException("Nenhum livro encontrado para: " + bookName);
+
         }
 
-        BookData bookData = resultsData.results().get(0);
-
-        return bookData;
+        return resultsData.results().get(0);
     }
 
-    public void searchBooksWeb() {
-        BookData bookData = getBookData();
+    public void searchBooksWeb(String bookName) {
+        BookData bookData = getBookData(bookName);
         Book book = BookMapper.toEntity(bookData);
-        helper.getBookRepository().save(book);
+        repository.save(book);
         System.out.println(book);
     }
 
     public void listAllBooks() {
-        List<Book> books = helper.getBookRepository().findAll();
+        List<Book> books = repository.findAll();
         books.forEach(System.out::println);
     }
 }
