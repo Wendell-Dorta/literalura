2fdd53f744da3b6bd8ecf0ca3733280a2b40b0a4
Wendell Dorta <wendelldorta.ws@outlook.com.br>
2025-07-30 21:36:55 -0300

    feat: Criação do BookService com método getBookData para buscar livro na API Gutendex

---
+++ src/main/java/br/com/alura_challenge/literalura/service/BookService.java
@@ -0,0 +1,44 @@
+package br.com.alura_challenge.literalura.service;
+
+import br.com.alura_challenge.literalura.dto.BookData;
+import br.com.alura_challenge.literalura.dto.BookResultsData;
+import br.com.alura_challenge.literalura.repository.BookRepository;
+import br.com.alura_challenge.literalura.util.ConsumptionApi;
+import br.com.alura_challenge.literalura.util.ConvertData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Scanner;
+
+@Service
+public class BookService {
+    private final Scanner scanner = new Scanner(System.in);
+    private final ConsumptionApi api;
+    private final ConvertData convertData;
+    private final String ENDERECO = "https://gutendex.com/books/?";
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    public BookService(ConsumptionApi api, ConvertData convertData) {
+        this.api = api;
+        this.convertData = convertData;
+    }
+
+    private BookData getBookData() {
+        System.out.println("Digite o nome de um livro para buscar");
+        var bookName = scanner.nextLine();
+
+        String url = ENDERECO + "search=" + bookName.replace(" ", "+");
+        var json = api.getData(url);
+        BookResultsData resultsData = convertData.getData(json, BookResultsData.class);
+
+        if (resultsData.results().isEmpty()) {
+            throw new RuntimeException("Nenhum livro encontrado para: " + bookName);
+        }
+
+        BookData bookData = resultsData.results().get(0);
+
+        return bookData;
+    }
+}
