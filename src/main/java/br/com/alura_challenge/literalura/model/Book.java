4ec7db0c51b3b4bb7b7ad39ecfb4a77fb19961f1
Wendell Dorta <wendelldorta.ws@outlook.com.br>
2025-07-30 21:29:39 -0300

    feat(book): criando toString

--- src/main/java/br/com/alura_challenge/literalura/model/Book.java
+++ src/main/java/br/com/alura_challenge/literalura/model/Book.java
@@ -3,6 +3,7 @@ package br.com.alura_challenge.literalura.model;
 import jakarta.persistence.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
 @Table(name="books")
@@ -13,7 +14,7 @@ public class Book {
     private String title;
     @ManyToMany(mappedBy = "books", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Author> authors;
-    @ManyToMany
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JoinTable(
             name = "book_language",
             joinColumns = @JoinColumn(name = "book_id"),
@@ -70,5 +71,39 @@ public class Book {
 
     public void setDownloadsNumber(Integer downloadsNumber) {
         this.downloadsNumber = downloadsNumber;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n-----------------------------------------------------------------\n");
+        sb.append("Título: ").append(this.title).append("\n");
+
+        sb.append("Autor: ");
+        if (authors != null && !authors.isEmpty()) {
+            String authorsStr = authors.stream()
+                    .map(author -> author.getName())
+                    .collect(Collectors.joining(", "));
+            sb.append(authorsStr);
+        } else {
+            sb.append("Nenhum autor");
+        }
+        sb.append("\n");
+
+        sb.append("Idiomas: ");
+        if (languages != null && !languages.isEmpty()) {
+            String languagesStr = languages.stream()
+                    .map(language -> language.getCode())
+                    .collect(Collectors.joining(", "));
+            sb.append(languagesStr);
+        } else {
+            sb.append("Nenhum idioma");
+        }
+        sb.append("\n");
+
+        sb.append("Número de Downloads: ").append(this.downloadsNumber).append("\n");
+        sb.append("-----------------------------------------------------------------");
+
+        return sb.toString();
     }
 }
