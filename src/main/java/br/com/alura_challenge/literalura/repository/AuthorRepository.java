8723378e79d30cee39b2d5285d3a523d3f86be33
Wendell Dorta <wendelldorta.ws@outlook.com.br>
2025-07-31 18:53:03 -0300

    fix(persistencia): Corrige LazyInitializationException na busca de autores

--- src/main/java/br/com/alura_challenge/literalura/repository/AuthorRepository.java
+++ src/main/java/br/com/alura_challenge/literalura/repository/AuthorRepository.java
@@ -3,12 +3,13 @@ package br.com.alura_challenge.literalura.repository;
 import br.com.alura_challenge.literalura.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
     Optional<Author> findByName(String name);
-    @Query("SELECT a FROM Author a WHERE a.birthYear >= :year AND a.deathYear <= :year")
-    List<Author> findAuthorsAliveInYear(String year);
+    @Query("SELECT a FROM Author a LEFT JOIN FETCH a.books WHERE a.birthYear <= :year AND (a.deathYear IS NULL OR a.deathYear >= :year)")
+    List<Author> findAuthorsAliveInYear(@Param("year") Integer year);
 }
