09a5c1b565792d0586d959a37b6f02af19d8a30c
Wendell Dorta <wendelldorta.ws@outlook.com.br>
2025-07-31 00:25:16 -0300

    fix(CommomServiceHelper): adicionando os repositorios como serviço comum

--- src/main/java/br/com/alura_challenge/literalura/service/CommonServiceHelper.java
+++ src/main/java/br/com/alura_challenge/literalura/service/CommonServiceHelper.java
@@ -1,5 +1,8 @@
 package br.com.alura_challenge.literalura.service;
 
+import br.com.alura_challenge.literalura.repository.AuthorRepository;
+import br.com.alura_challenge.literalura.repository.BookRepository;
+import br.com.alura_challenge.literalura.repository.LanguageRepository;
 import br.com.alura_challenge.literalura.util.ConsumptionApi;
 import br.com.alura_challenge.literalura.util.ConvertData;
 import org.springframework.stereotype.Component;
@@ -12,10 +15,20 @@ public class CommonServiceHelper {
     private final ConsumptionApi api;
     private final ConvertData convertData;
     private final String ENDERECO = "https://gutendex.com/books/?";
+    private final AuthorRepository authorRepository;
+    private final LanguageRepository languageRepository;
+    private final BookRepository bookRepository;
 
-    public CommonServiceHelper(ConsumptionApi api, ConvertData convertData) {
+    public CommonServiceHelper(ConsumptionApi api,
+                               ConvertData convertData,
+                               BookRepository bookRepository,
+                               LanguageRepository languageRepository,
+                               AuthorRepository authorRepository) {
         this.api = api;
         this.convertData = convertData;
+        this.bookRepository = bookRepository;
+        this.authorRepository = authorRepository;
+        this.languageRepository = languageRepository;
     }
 
     public Scanner getScanner() {
@@ -32,6 +45,18 @@ public class CommonServiceHelper {
 
     public String getEndereco() {
         return ENDERECO;
+    }
+
+    public AuthorRepository getAuthorRepository() {
+        return authorRepository;
+    }
+
+    public LanguageRepository getLanguageRepository() {
+        return languageRepository;
+    }
+
+    public BookRepository getBookRepository() {
+        return bookRepository;
     }
 
 }
