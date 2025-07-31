18f12dd22b79e97e084012b7f0becae8513735e1
Wendell Dorta <wendelldorta.ws@outlook.com.br>
2025-07-31 19:08:24 -0300

    feat: criando método findByLanguagesCode

--- src/main/java/br/com/alura_challenge/literalura/repository/BookRepository.java
+++ src/main/java/br/com/alura_challenge/literalura/repository/BookRepository.java
@@ -3,8 +3,10 @@ package br.com.alura_challenge.literalura.repository;
 import br.com.alura_challenge.literalura.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Integer> {
     Optional<Book> findByTitleIgnoreCase(String title);
+    List<Book> findByLanguagesCode(String language);
 }
