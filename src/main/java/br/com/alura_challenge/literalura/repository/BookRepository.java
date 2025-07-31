3a032a57a2fdfc7418927d7f5506d4a8da0a7b73
Wendell Dorta <wendelldorta.ws@outlook.com.br>
2025-07-31 16:01:19 -0300

    feat(BookRepository): adicionando método para buscar no banco pelo titulo do livro

--- src/main/java/br/com/alura_challenge/literalura/repository/BookRepository.java
+++ src/main/java/br/com/alura_challenge/literalura/repository/BookRepository.java
@@ -3,5 +3,8 @@ package br.com.alura_challenge.literalura.repository;
 import br.com.alura_challenge.literalura.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BookRepository extends JpaRepository<Book, Integer> {
+    Optional<Book> findByTitleIgnoreCase(String title);
 }
