5d5e93881c9f405179e32951a10f832c883c5362
Wendell Dorta <wendelldorta.ws@outlook.com.br>
2025-07-31 18:41:45 -0300

    feat(AuthorRepository): criando a query findAuthorsAliveInYear

--- src/main/java/br/com/alura_challenge/literalura/repository/AuthorRepository.java
+++ src/main/java/br/com/alura_challenge/literalura/repository/AuthorRepository.java
@@ -2,9 +2,13 @@ package br.com.alura_challenge.literalura.repository;
 
 import br.com.alura_challenge.literalura.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
     Optional<Author> findByName(String name);
+    @Query("SELECT a FROM Author a WHERE a.birthYear >= :year AND a.deathYear <= :year")
+    List<Author> findAuthorsAliveInYear(String year);
 }
